efee05501c6de2f406586541f262dcbf2bf517b4
murtazinram <murtazinram@gmail.com>
2020-10-11 23:09:12 +0300

    feat: solved brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = str.split('')
+    for (let i = 0; i < str.length - 1; i++) {
+        for (let item of bracketsConfig) {
+            if (str[i] === item[0] && str[i + 1] === item[1]) {
+                str.splice(i, 2)
+                i -= 2
+            }
+        }
+    }
+    return str.length === 0;
 }
